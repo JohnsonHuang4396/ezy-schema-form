4751625d075b5e0db14e63b77422c8bc133cde05
黄启浩 <huangqh@xianghe.com>
2023-07-26 15:09:33 +0800

    feat: use element-plus

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,5 +1,6 @@
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import vitePluginImp from 'vite-plugin-imp'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
@@ -14,6 +15,23 @@ export default defineConfig({
       dirs: ['./src/utils', './src/apis'],
       dts: './src/typings/auto-import.d.ts',
       vueTemplate: true
+    }),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'element-plus',
+          libDirectory: 'es/components',
+          nameFormatter: (name: string) => {
+            return `${name.replace('el-', '')}/index.mjs`
+          },
+          style(name) {
+            return `element-plus/es/components/${name.replace(
+              'el-',
+              ''
+            )}/style/css.mjs`
+          }
+        }
+      ]
     })
   ],
   resolve: {
