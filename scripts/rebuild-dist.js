0176f6be4184665bf4d921c53974b9b711dabdf2
黄启浩 <huangqh@xianghe.com>
2023-08-04 16:45:21 +0800

    fix: fix build script error in windows platform

--- scripts/rebuild-dist.js
+++ scripts/rebuild-dist.js
@@ -1,15 +1,9 @@
 import fs from 'fs'
-import * as dartSass from 'sass'
-import gulpSass from 'gulp-sass'
-import gulp from 'gulp'
-import autoPrefixer from 'gulp-autoprefixer'
-import { spawn } from 'child_process'
+import { fileURLToPath } from 'url'
+import { exec } from 'child_process'
 import { resolve, dirname } from 'path'
 
-const sass = gulpSass(dartSass)
-
-const __filename = new URL(import.meta.url).pathname
-const __dirname = dirname(__filename)
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const componentsPath = resolve(__dirname, '../packages/core/components')
 export const pkgPath = resolve(__dirname, '../packages/core')
@@ -17,15 +11,10 @@ export const pkgPath = resolve(__dirname, '../packages/core')
 const stayFile = ['package.json', 'README.md']
 
 export function run(command, path) {
-  const [cmd, ...args] = command.split(' ')
   return new Promise((resolve, reject) => {
-    const app = spawn(cmd, args, {
-      cwd: path,
-      stdio: 'inherit',
-      shell: true //mac不需要开启，windows下git base需要开启支持
-    })
-    //执行完毕关闭并resolve
-    app.on('close', resolve)
+    const childProcess = exec(`${path} ${command}`)
+
+    childProcess.on('close', resolve)
   })
 }
 
@@ -51,23 +40,3 @@ export const delPath = async path => {
   } else return
   if (path !== `${pkgPath}/es` || path !== `${pkgPath}/lib`) fs.rmdirSync(path)
 }
-
-async function buildStyle() {
-  return gulp
-    .src(`${pkgPath}/components/**/style/**.scss`)
-    .pipe(sass())
-    .pipe(autoPrefixer())
-    .pipe(gulp.dest(`${pkgPath}/es/components`))
-    .pipe(gulp.dest(`${pkgPath}/lib/components`))
-}
-
-async function buildComponents() {
-  await run(`pnpm -w build`, pkgPath)
-  await buildStyle()
-}
-
-export default gulp.series(
-  async () => delPath(`${pkgPath}/es`),
-  async () => delPath(`${pkgPath}/lib`),
-  async () => buildComponents()
-)
