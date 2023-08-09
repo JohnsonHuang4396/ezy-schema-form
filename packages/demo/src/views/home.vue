35e5c4d97ff94638d9b512dacce7c943d5a5ba3c
黄启浩 <huangqh@xianghe.com>
2023-08-09 11:26:35 +0800

    feat: add type hint

--- packages/demo/src/views/home.vue
+++ packages/demo/src/views/home.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const age = ref(12)
+
+  function handleClick() {
+    age.value += 1
+  }
+</script>
 
 <template>
-  <Test />
+  <Test
+    :age="age"
+    @click="handleClick"
+  />
 </template>
 
 <style lang="scss" scoped></style>
