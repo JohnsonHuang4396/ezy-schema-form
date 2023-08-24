300ac5579a4a315ff1af882d0ee72faa450fcf68
黄启浩 <huangqh@xianghe.com>
2023-08-24 14:57:38 +0800

    feat: demo basic complete

--- packages/demo/src/views/basic.vue
+++ packages/demo/src/views/basic.vue
@@ -1,19 +1,50 @@
 <template>
   <div un-h-full>
-    <SchemaForm :config="schema" />
+    <SchemaForm
+      ref="schemaFormRef"
+      :config="schema"
+    />
+    <el-button
+      type="primary"
+      @click="handleValidate"
+    >
+      validate
+    </el-button>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { SchemaForm } from '@vue3-form/core'
+  import { ElButton } from 'element-plus'
   import type { Vue3FormProps } from '@vue3-form/core/types'
 
   const schema = ref<Vue3FormProps>({
-    classList: ['grid', ''],
+    classList: [],
     schema: [
-      { field: 'name', label: '名称', component: { comp: 'input' } },
+      {
+        field: 'name',
+        label: '名称',
+        defaultValue: 'hqh',
+        rule: { required: true, message: '请输入名称', trigger: ['blur'] },
+        component: {
+          comp: 'input',
+          props: { type: 'textarea', maxlength: 100, disabled: false }
+        }
+      },
       { field: 'age', label: '年龄', component: { comp: 'input-number' } }
     ]
   })
+
+  const schemaFormRef = ref<InstanceType<typeof SchemaForm>>()
+
+  async function handleValidate() {
+    if (!schemaFormRef.value) return
+
+    const [, error] = await schemaFormRef.value.validate()
+    if (error) return
+
+    console.log('pass')
+  }
 </script>
 
 <style lang="scss" scoped></style>
