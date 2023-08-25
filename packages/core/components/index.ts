7e3b9bc25cd49d1aefba64c224f95b9641ae2aca
JohnsonHuang4396 <hqhEmail0802@163.com>
2023-08-25 23:19:42 +0800

    chore: add TODO

--- packages/core/components/index.ts
+++ packages/core/components/index.ts
@@ -1,2 +1,8 @@
+// TODO: 兼容其他组件库，将element-plus解耦(可以通过全局注册函数和useForm注册所需要的element-plus组件,未注册前默认没有组件)
+// TODO: 处理事件，模拟v-model，区分受控和非受控状态
+// TODO: 处理select-option
+// TODO: 处理checkbox-group
+// TODO: 处理radio-group
+
 export * from './SchemaForm'
 export * from './Custom'
