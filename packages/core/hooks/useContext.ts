d3f5c7195a3dc5f2f3ce325018d93a722fc538ba
黄启浩 <huangqh@xianghe.com>
2023-09-05 11:25:02 +0800

    other: useContext insurance

--- packages/core/hooks/useContext.ts
+++ packages/core/hooks/useContext.ts
@@ -2,6 +2,8 @@ const symbolMap = new Map()
 
 export function useContext() {
   function set(key: string) {
+    if (symbolMap.has(key)) return
+
     const provideSymbol = Symbol(key)
     symbolMap.set(key, provideSymbol)
   }
