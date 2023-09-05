2cd23afdab735027488c14d2266b6e45af13f65f
黄启浩 <huangqh@xianghe.com>
2023-09-05 16:53:57 +0800

    other: update controlled mode

--- packages/core/components/SchemaForm/SchemaFormItem.vue
+++ packages/core/components/SchemaForm/SchemaFormItem.vue
@@ -4,6 +4,7 @@
   import { useComponents } from '../../hooks'
   import { useContext } from '../../hooks/useContext'
   import { VUE3_FORM_PROVIDE_KEY } from '../../constant'
+  import { initDefaultValue } from '../../utils'
   import { upperFirstLetter } from '../../utils/common'
   import type { Vue3FormItem } from '../../types'
   import type { Ref, PropType } from 'vue'
@@ -31,6 +32,9 @@
     setup(props) {
       const formModel = inject(get(VUE3_FORM_PROVIDE_KEY)) as Ref<any>
 
+      /**
+       * @desc 格式化actions
+       **/
       function formatActions(actions: Record<string, any>) {
         return Reflect.ownKeys(actions).reduce<Record<string, any>>((acc, key) => {
           const action = actions[key as string]
@@ -39,17 +43,16 @@
         }, {})
       }
 
+      /**
+       * @desc 初始化受控函数
+       */
       function initCtlAction(ctl: boolean) {
         const { schema } = props
         const { updateModelValue, component } = schema
-        const { comp } = component
-
-        const event = comp === 'input' ? 'input' : 'change'
-        const onUpdate = `on${upperFirstLetter(event)}`
 
         const updateEvent: Record<string, any> = {}
 
-        updateEvent[onUpdate] = ctl
+        updateEvent['onUpdate:modelValue'] = ctl
           ? function (val: any) {
               formModel.value[schema.field] = val
             }
@@ -63,7 +66,11 @@
         const { component, ctl = true } = schema
         const { attrs = {}, actions = {} } = component
 
-        let modelValue: Record<string, any> = { modelValue: formModel.value[schema.field] }
+        formModel.value[schema.field] ??= initDefaultValue(schema)
+
+        const modelValue: Record<string, any> = {
+          modelValue: formModel.value[schema.field]
+        }
 
         const on = { ...initCtlAction(ctl), ...formatActions(actions) }
 
